9cad9d700ec32ebd8c0c1725b1c94390f6ca658a
german deev <geradeev57@gmail.com>
2025-04-19 23:18:58 +0300

    added Category, Limit and Transaction in admin panel

--- backend/admin_panel/admin.py
+++ backend/admin_panel/admin.py
@@ -1,6 +1,6 @@
 from sqladmin import ModelView
 
-from core.models import User
+from core.models import Category, Limit, Transaction, User
 
 
 class UserAdmin(ModelView, model=User):
@@ -9,4 +9,33 @@ class UserAdmin(ModelView, model=User):
     column_list = [
         User.id, User.telegram_id, User.username,
         User.role, User.password_hash, User.categories,
+    ]
+
+
+class CategoryAdmin(ModelView, model=Category):
+    """Класс категории в админке."""
+
+    column_list = [
+        Category.id, Category.name, Category.user_id,
+        Category.user, Category.transactions,
+    ]
+
+
+class TransactionAdmin(ModelView, model=Transaction):
+    """Класс операции в админке."""
+
+    column_list = [
+        Transaction.id, Transaction.amount, Transaction.type,
+        Transaction.description, Transaction.user_id,
+        Transaction.category_id, Transaction.user, Transaction.category,
+    ]
+
+
+class LimitAdmin(ModelView, model=Limit):
+    """Класс лимита в админке."""
+
+    column_list = [
+        Limit.id, Limit.user_id, Limit.category_name,
+        Limit.amount, Limit.period, Limit.last_renewed,
+        Limit.user,
     ]
--- backend/start_admin.py
+++ backend/start_admin.py
@@ -2,15 +2,21 @@ import uvicorn
 from fastapi import FastAPI
 from sqladmin import Admin
 
-from admin_panel.admin import UserAdmin
+from admin_panel.admin import (
+    CategoryAdmin,
+    LimitAdmin,
+    TransactionAdmin,
+    UserAdmin,
+)
 from core.database import engine
 
 app = FastAPI()
 admin = Admin(app, engine)
 
-
 admin.add_view(UserAdmin)
-
+admin.add_view(CategoryAdmin)
+admin.add_view(TransactionAdmin)
+admin.add_view(LimitAdmin)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
